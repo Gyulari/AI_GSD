1c8a7d8195d6d571ee405c24187e5c909d4b432c
KimGTs <newstartak1@gmail.com>
2022-10-20 20:22:39 +0900

    공 튕기기 기능, 트레일 렌더러 간단 구현

---
+++ AI_GSD/Assets/02. Scripts/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * 10.0f, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
